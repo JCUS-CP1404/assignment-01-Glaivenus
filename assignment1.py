2a1d224fb536297da902eaf5d845507cbb13a3ce
Glaivenus <taokexin0722@gmail.com>
2019-12-08 03:36:11 +0800

    change the URL

--- assignment1.py
+++ assignment1.py
@@ -2,7 +2,7 @@
 Replace the contents of this module docstring with your own details
 Name: Tao Kexin
 Date started: 28/11/2019
-GitHub URL: https://github.com/JCUS-CP1404/assignment-01-Glaivenus
+GitHub URL: https://github.com/JCUS-CP1404/assignment-01-Glaivenus.git
 """
 
 MENU = """Menu:
